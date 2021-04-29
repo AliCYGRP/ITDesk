4e8cf9555fbc0e594c76963f23cc28742c6e285b
Syed Ali Hasan <syed.hasan@cybergindia.local>
2021-04-29 21:35:33 +0530

    Saksham Ali: Backend

--- ITDesk/ITDesk/Models/DeviceInfo.cs
+++ ITDesk/ITDesk/Models/DeviceInfo.cs
@@ -8,13 +8,14 @@ namespace ITDesk.Models
         public int DeviceId { get; set; }
         public string UniqueCode { get; set; }
         public string DeviceName { get; set; }
-        public string DeviceType { get; set; }
+        public int CategoryId { get; set; }
         public int? EmployeeId { get; set; }
         public DateTime AssignedDate { get; set; }
         public bool? IsAssigned { get; set; }
         public string AssignedBy { get; set; }
         public string QrCode { get; set; }
 
+        public DeviceCategory Category { get; set; }
         public EmployeeInfo Employee { get; set; }
     }
 }
--- ITDesk/ITDesk/Startup.cs
+++ ITDesk/ITDesk/Startup.cs
@@ -1,13 +1,20 @@
-﻿using ITDesk.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITDesk.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace ITDesk
 {
